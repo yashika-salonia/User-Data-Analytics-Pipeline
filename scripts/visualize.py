7413b92e0345f3dca39bd0137df955df6477f0cd
Yashika Salonia <146370396+yashika-salonia@users.noreply.github.com>
2026-04-17 20:34:18 +0530

    fix: few fixes in graphs

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -3,21 +3,24 @@ import matplotlib.pyplot as plt
 def plot_data(df1, df2):
 
     color1= "#932ED6"
-    color2 = "#4F46E5"
+    color2 = "#E7224C"
 
     # Graph 1
     plt.figure(figsize=(8,5))
     plt.bar(df1["subject"], df1["avg_session"], color=color1)
+    plt.xlabel("Subject")
+    plt.ylabel("Avg Session Time")
     plt.title("Avg Session Time per Subject")
     plt.savefig("output/charts/sql_subject.png")
     plt.close()
 
     # Graph 2
     plt.figure(figsize=(8,5))
-    plt.scatter(df2["distraction_count"], df2["avg_time"], color=color2)
+    plt.scatter(df2["distraction_count"], df2["avg_time"], color=color2, marker='o')
+    plt.plot(df2["distraction_count"], df2["avg_time"], color=color2, linestyle='--', alpha=0.5)
     plt.xlabel("Distraction Count")
-    plt.ylabel("Session Time")
-    plt.title("Distraction vs Session Time")
+    plt.ylabel("Avg Session Time")
+    plt.title("Distraction vs Avg Session Time")
     plt.savefig("output/charts/sql_distraction.png")
     plt.close()
 
